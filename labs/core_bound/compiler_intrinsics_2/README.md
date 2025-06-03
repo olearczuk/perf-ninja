ae5cb24bb5ca3e57b56a9a38c3b4437accc9353a
olearczuk <olearczuk.szymon@gmail.com>
2025-06-03 16:02:18 +0400

    solution for compiler_intrinsics_2 lab

--- labs/core_bound/compiler_intrinsics_2/README.md
+++ labs/core_bound/compiler_intrinsics_2/README.md
@@ -1,5 +1,3 @@
-[<img src="../../../img/CompIntrin2.png">](https://www.youtube.com/watch?v=0WUihFxjzSE&list=PLRWO2AL1QAV6bJAU2kgB4xfodGID43Y5d)
-
 This is a second lab about using [compiler intrinsics](https://en.wikipedia.org/wiki/Intrinsic_function) to speed up parts of the code, where compilers fail to generate optimal code.
 
 The task of this lab assignment is to find the longest line in a file. There is a way to find end-of-line characters in a parallel way if you utilize compiler intrinsics.
@@ -11,3 +9,23 @@ The idea for this lab was proposed by Yuriy Lyfenko (@obender12).
 Co-authored-by: Andrew Evstyukhin (@andrewevstyukhin)
 
 Co-authored-by: Jakub Beránek (@Kobzol)
+
+
+## Solution
+The problem with naive approach is that it checks for newlines one byte at a time.<br/>
+Instead, we can check multiple bytes at once for newlines, treat the result as a bitmap and calculate distances between consecutive newlines using the bitmap.<br/>
+Using comipler intrinsics to trigger SIMD operations is the perfect candidate.
+
+## Benchmark
+Using AVX2-based approach results in ~8x speedup.
+```bash
+Benchmark                   Time             CPU      Time Old      Time New       CPU Old       CPU New
+--------------------------------------------------------------------------------------------------------
+bench1                   -0.8878         -0.8878           306            34           306            34
+Benchmark                   Time             CPU      Time Old      Time New       CPU Old       CPU New
+--------------------------------------------------------------------------------------------------------
+bench1                   -0.8845         -0.8845           306            35           306            35
+Benchmark                   Time             CPU      Time Old      Time New       CPU Old       CPU New
+--------------------------------------------------------------------------------------------------------
+bench1                   -0.8836         -0.8837           306            36           306            36
+```
--- labs/core_bound/compiler_intrinsics_2/solution.cpp
+++ labs/core_bound/compiler_intrinsics_2/solution.cpp
@@ -1,6 +1,9 @@
 #include "solution.hpp"
-#include <iostream>
 
+#include <immintrin.h>
+
+#include <cstdint>
+#include <iostream>
 // Find the longest line in a file.
 // Implementation uses ternary operator with a hope that compiler will
 // turn it into a CMOV instruction.
@@ -12,7 +15,7 @@ if (s == '\n') {
 } else {
   curLineLength++;
 }*/
-unsigned solution(const std::string &inputContents) {
+unsigned solutionOld(const std::string& inputContents) {
   unsigned longestLine = 0;
   unsigned curLineLength = 0;
 
@@ -22,4 +25,54 @@ unsigned solution(const std::string &inputContents) {
   }
 
   return longestLine;
+}
+
+unsigned solutionAVX2(const std::string& inputContents) {
+  const __m256i newline = _mm256_set1_epi8('\n');
+
+  int previous_newline = 0;
+  int max_length = 0;
+  size_t i = 0;
+  int len = inputContents.size();
+  for (; i + 32 <= len; i += 32) {
+    __m256i chunk = _mm256_loadu_si256((const __m256i*)&inputContents[i]);
+    // Check for newline characters in the chunk
+    __m256i cmp = _mm256_cmpeq_epi8(chunk, newline);
+    // Keep in mind that bit N is set to 1 if the N-th byte in the chunk is
+    // equal to '\n'.
+    // This means ordering is "flipped" compared to the original
+    // string. For example, if the first byte is '\n', the least significant bit
+    // of the mask will be set.
+    // That's why __builtin_ctz is used (trailing 0s) to find positions of \n.
+    unsigned int mask = _mm256_movemask_epi8(cmp);
+
+    // Pop all 1s from the mask and calculate line lengths
+    while (mask != 0) {
+      int pos = __builtin_ctz(mask) + i;
+      max_length = std::max(max_length, pos - previous_newline);
+      previous_newline = pos + 1;
+
+      mask &= mask - 1;
+    }
+  }
+
+  // Process remaining bytes (if any) without SIMD
+  for (; i < len; i++) {
+    if (inputContents[i] == '\n') {
+      int length = i - previous_newline;
+      previous_newline = i + 1;
+      max_length = std::max(max_length, length);
+    }
+  }
+
+  max_length = std::max(max_length, len - previous_newline);
+  return static_cast<unsigned>(max_length);
+}
+
+unsigned solution(const std::string& inputContents) {
+#ifdef SOLUTION
+  return solutionAVX2(inputContents);
+#else
+  return solutionOld(inputContents);
+#endif
 }
